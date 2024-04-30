279018eac6b0492851d5b0b3fc9310880bc8767c
Ignacio J. Perez Portal <5990@protonmail.com>
2024-04-30 08:22:17 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CSP-Hash-Generator
 Javascript to quickly generate CSP hashes for all script/style elements in a website.
+
+1. Copy the source code of [main.js](main.js)
+2. Open the website you want to generate the SHA-256 integrity hashes for.
+3. Open the browser's developer console.
+4. Paste the code into the developer console.
+5. Press enter.
+
+The results will be logged to the browser's console via console.log in messages like:
+```
+Style Hash for: <style>
+Code snippet:
+
+			:where([data-hydro-view*="pull-request-hovercard-hover"] ~ .d-flex.mt-2):not(.rgh-seen--8093118031) {
+				animation: 1ms rgh-selector-observer;
+
+The SHA-256 for this style is: sha256-iYiB5NAARSnEd19sfwObvGTjyh4xG4zFPcenZcpB40I=
+```
---
+++ main.js
@@ -0,0 +1,39 @@
+// Get all script elements
+const scripts = document.getElementsByTagName("script");
+
+// Loop through each script element
+for (let i = 0; i < scripts.length; i++) {
+    const script = scripts[i];
+    const scriptLines = script.innerHTML.split('\n').slice(0, 3).join('\n\t'); // Get the first 3 lines of the script
+    const enc = new TextEncoder();
+    const data = enc.encode(script.innerHTML); // Encode script content
+    crypto.subtle.digest('SHA-256', data).then(function(val){
+        const digest = ["sha256", _arrayBufferToBase64(val)].join('-');
+        console.log(`Script Hash for:`, script, `\nCode snippet:\n${scriptLines}\n\nThe SHA-256 for this script is: ${digest}\n`);
+    });
+}
+
+// Get all style elements
+const styles = document.getElementsByTagName("style");
+
+// Loop through each style element
+for (let i = 0; i < styles.length; i++) {
+    const style = styles[i];
+    const styleLines = style.innerHTML.split('\n').slice(0, 3).join('\n\t'); // Get the first 3 lines of the style
+    const enc = new TextEncoder();
+    const data = enc.encode(style.innerHTML); // Encode style content
+    crypto.subtle.digest('SHA-256', data).then(function(val){
+        const digest = ["sha256", _arrayBufferToBase64(val)].join('-');
+        console.log(`Style Hash for:`, style, `\nCode snippet:\n${styleLines}\n\nThe SHA-256 for this style is: ${digest}\n`);
+    });
+}
+
+function _arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
